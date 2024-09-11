2e173756936161892c51fd4c8f2802d7b275434c
phucducz <phucducduongvinh05102003@gmail.com>
2024-09-11 09:45:33 +0700

    [FEAT] handle create new job category and create new job categories list

--- src/dto/job_categories/create-job_category.dto.ts
+++ src/dto/job_categories/create-job_category.dto.ts
@@ -1,1 +1,11 @@
-export class CreateJobCategoryDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateJobCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description: string;
+}
