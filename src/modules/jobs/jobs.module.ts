38b6c361b1839e5170d53afed1baea0c638979ff
phucducz <phucducduongvinh05102003@gmail.com>
2024-09-13 11:22:28 +0700

    [FEAT] handle get job list and get job by id

--- src/modules/jobs/jobs.module.ts
+++ src/modules/jobs/jobs.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Job } from 'src/entities/job.entity';
+
 import { JobsService } from '../../services/jobs.service';
 import { JobsController } from './jobs.controller';
+import { JobsRepository } from './jobs.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Job])],
   controllers: [JobsController],
-  providers: [JobsService],
+  providers: [JobsService, JobsRepository],
+  exports: [JobsService],
 })
 export class JobsModule {}
