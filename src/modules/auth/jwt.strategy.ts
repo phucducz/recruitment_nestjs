a1627841eb528f05b99cf3f545d7ec0acf4b8997
phucducz <phucducduongvinh05102003@gmail.com>
2024-09-24 11:31:27 +0700

    [FIX] handle get refresh token from cookies

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -1,15 +1,21 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(@Inject(ConfigService) private configService: ConfigService) {
-    console.log('JWT_SECRET:', configService.get<string>('JWT_SECRET'));
-
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return (
+            request?.cookies?.jwt ||
+            ExtractJwt.fromAuthHeaderAsBearerToken()(request)
+          );
+        },
+      ]),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('JWT_SECRET'),
     });
