a4bde1337066754f341dc5952b16e4450ee1fe51
phucducz <phucducduongvinh05102003@gmail.com>
2024-10-10 21:41:21 +0700

    [FEAT] handle apply for a job with unuploaded CV

--- src/modules/users_jobs/users_jobs.controller.ts
+++ src/modules/users_jobs/users_jobs.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
@@ -8,30 +9,105 @@ import {
   Post,
   Request,
   Res,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { Response } from 'express';
 
+import { FileInterceptor } from '@nestjs/platform-express';
 import { CreateUsersJobDto } from 'src/dto/users_jobs/create-users_job.dto';
 import { UpdateUsersJobDto } from 'src/dto/users_jobs/update-users_job.dto';
+import { CloudinaryService } from 'src/services/cloudinary.service';
+import { CurriculumVitaesService } from 'src/services/curriculum_vitaes.service';
 import { UsersJobsService } from '../../services/users_jobs.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { cloudinaryStorage } from '../cloudinary/cloudinary-storage.config';
 
 @Controller('users-jobs')
 export class UsersJobsController {
-  constructor(private readonly usersJobsService: UsersJobsService) {}
+  constructor(
+    private readonly usersJobsService: UsersJobsService,
+    private readonly cloudinaryService: CloudinaryService,
+    private readonly curriculumVitaesService: CurriculumVitaesService,
+  ) {}
+
+  // @UseGuards(JwtAuthGuard)
+  // @Post()
+  // async create(
+  //   @Body() createUsersJobDto: CreateUsersJobDto,
+  //   @Res() res: Response,
+  //   @Request() request: any,
+  // ) {
+  //   try {
+  //     const result = await this.usersJobsService.create({
+  //       createBy: request.user.userId,
+  //       variable: createUsersJobDto,
+  //     });
+
+  //     if (!result?.jobsId)
+  //       return res
+  //         .status(401)
+  //         .json({ message: 'Ứng tuyển không thành công!', statusCode: 401 });
+
+  //     return res
+  //       .status(200)
+  //       .json({ message: 'Ứng tuyển thành công!', statusCode: 200 });
+  //   } catch (error) {
+  //     return res
+  //       .status(500)
+  //       .json({ message: error?.message ?? error, statusCode: 500 });
+  //   }
+  // }
 
   @UseGuards(JwtAuthGuard)
   @Post()
+  @UseInterceptors(FileInterceptor('file', { storage: cloudinaryStorage }))
   async create(
-    @Body() createUsersJobDto: CreateUsersJobDto,
+    @UploadedFile() file: Express.Multer.File,
+    @Body() createUsersJobDto: any,
     @Res() res: Response,
     @Request() request: any,
   ) {
     try {
-      const result = await this.usersJobsService.create({
-        createBy: request.user.userId,
-        variable: createUsersJobDto,
+      let data = {
+        ...createUsersJobDto,
+        jobsId: +createUsersJobDto?.jobsId,
+      } as CreateUsersJobDto;
+      const createBy = request.user.userId;
+
+      const isApplied = await this.usersJobsService.isApplied({
+        jobsId: data.jobsId,
+        usersId: createBy,
+      });
+
+      if (isApplied)
+        throw new BadRequestException(
+          'Bạn đã ứng tuyển công việc này, hãy kiểm tra danh sách công việc bạn đã ứng tuyển!',
+        );
+
+      if (file) {
+        const uploadResult = await this.cloudinaryService.uploadFileByPath(
+          file.path,
+        );
+        const saveCVResult = await this.curriculumVitaesService.create({
+          createBy,
+          variable: { url: uploadResult.secure_url },
+        });
+
+        if (!saveCVResult?.id) {
+          return res.status(401).json({
+            message: `Lưu dữ liệu vào cơ sở dữ liệu thất bại. Tải file không thành công!`,
+            statusCode: 401,
+          });
+        }
+
+        data.curriculumVitaeURL = uploadResult.secure_url;
+      }
+
+      const result = await this.usersJobsService.aplly({
+        createBy,
+        variable: data,
       });
 
       if (!result?.jobsId)
@@ -43,6 +119,7 @@ export class UsersJobsController {
         .status(200)
         .json({ message: 'Ứng tuyển thành công!', statusCode: 200 });
     } catch (error) {
+      if (error instanceof BadRequestException) throw error;
       return res
         .status(500)
         .json({ message: error?.message ?? error, statusCode: 500 });
--- src/modules/users_jobs/users_jobs.module.ts
+++ src/modules/users_jobs/users_jobs.module.ts
@@ -4,6 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersJob } from 'src/entities/users_job.entity';
 import { UsersJobsService } from '../../services/users_jobs.service';
 import { AuthModule } from '../auth/auth.module';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
+import { CurriculumVitaesModule } from '../curriculum_vitaes/curriculum_vitaes.module';
 import { RefreshTokenModule } from '../refresh_token/refresh_token.module';
 import { UsersJobsController } from './users_jobs.controller';
 import { UsersJobRepository } from './users_jobs.repository';
@@ -13,6 +15,8 @@ import { UsersJobRepository } from './users_jobs.repository';
     TypeOrmModule.forFeature([UsersJob]),
     RefreshTokenModule,
     AuthModule,
+    CloudinaryModule,
+    CurriculumVitaesModule,
   ],
   controllers: [UsersJobsController],
   providers: [UsersJobsService, UsersJobRepository],
