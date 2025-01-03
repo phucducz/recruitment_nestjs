9aca85cb56363204d575ed8b383150435b18faf3
PanDaIT03 <daiphucduongvinh203@gmail.com>
2025-01-03 12:14:09 +0700

    feat: add payload

--- src/dto/roles/create-role.dto.ts
+++ src/dto/roles/create-role.dto.ts
@@ -1,7 +1,11 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateRoleDto {
   @IsString()
   @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsOptional()
+  description: string;
 }
